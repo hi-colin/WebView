85b0dff7e41672a8189909a61bcf70f4a3486d2a
Colin <811687790@qq.com>
2020-10-22 09:36:21 +0800

    init

---
+++ app/src/main/java/cn/hicolin/webview/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.hicolin.webview.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.hicolin.webview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
