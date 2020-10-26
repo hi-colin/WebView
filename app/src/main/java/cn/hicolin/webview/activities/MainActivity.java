4bdc5b4a7d13f3a4c7105ab48fceb85af53a6708
Colin <811687790@qq.com>
2020-10-26 14:14:06 +0800

    'fix'

--- app/src/main/java/cn/hicolin/webview/activities/MainActivity.java
+++ app/src/main/java/cn/hicolin/webview/activities/MainActivity.java
@@ -1,26 +1,16 @@
 package cn.hicolin.webview.activities;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.blankj.utilcode.util.ToastUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import cn.hicolin.webview.R;
 import cn.hicolin.webview.fragments.CenterFragment;
